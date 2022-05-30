6e3f269a4c8ec34af48afb995183cc2f011152cd
Ryland Taniguchi <ryland0000@gmail.com>
2022-05-30 00:26:02 -0700

    nested for loop

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-function naiveStringSearch(string) {}
+function naiveStringSearch(long, short) {
+  let longArr = [];
+  let longChar = long.split("");
+  longArr.push(longChar);
+  let shortArr = [];
+  let shortChar = short.split("");
+  shortArr.push(shortChar);
+  for (let i = 0; i < longArr.length; i++) {
+    for (let j = 0; i < shortArr.length; j++) {
+      if (longArr[i] !== shortArr[j]) return;
+    }
+  }
+}
